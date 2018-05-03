3173fea49009cfc8148acb3ba5ad6756c35d86a5
qubingqing <qubingqing@baidu.com>
2018-05-03 16:43:48 +0800

    minor update on head comment

--- init.el
+++ init.el
@@ -1,52 +1,53 @@
-;;;; DESCRIPTION
-;;;;+-----------+
-;;;; A super light-weighted emacs one-page conifg that implemented
-;;;; minimum requirement of programming on language like c++/python
-;;;;
-;;;; BRIEF FUNCTION LIST
-;;;;+-------------------+
-;;;; * Theme Setting
-;;;;   - zenburn
-;;;; * Basic and Classical GUI Setting
-;;;;   - common useless frame elements suppress
-;;;;   - cursor style & positioning visualization
-;;;; * Emac Functionality
-;;;;   - ido/smex/window-switch etc..
-;;;; * Editting Settings
-;;;;   - auto-complete/pairing/undo-tree/ace-jump-mode/100 column indicator/multiple-cursor
-;;;; * Programming Settings
-;;;;   - Company
-;;;;   - C/C++ Coding Styles (and very basic settings)
-;;;;   - Python (very basic settings)
-;;;; * Extensions
-;;;;
-;;;; LICENSE
-;;;;+-------+
-;;;; Copyright (c) 2018-2019 Bingqing Qu <sylar.qu@gmail.com>
-;;;; All rights reserved.
-;;;; Redistribution and use in source and binary forms, with or without
-;;;; modification, are permitted provided that the following conditions are
-;;;; met:
-;;;;     * Redistributions of source code must retain the above copyright
-;;;; notice, this list of conditions and the following disclaimer.
-;;;;     * Redistributions in binary form must reproduce the above copyright
-;;;; notice, this list of conditions and the following disclaimer in the
-;;;; documentation and/or other materials provided with the distribution.
-;;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-;;;; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-;;;; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-;;;; A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-;;;; HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-;;;; SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-;;;; LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-;;;; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-;;;; THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-;;;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-;;;; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-;;;;
-;;;; ----------------------------------------------------------------------
-;;;; Author : Bingqing Qu
-;;;; Version: 0.0
+;; init.el -- main initial config file superlight-emacs
+
+;; --- Description:
+;; A super light-weighted emacs one-page conifg that implemented
+;; minimum requirement of programming on language like c++/python
+
+;; --- Brief Function List:
+;; * Theme Setting
+;;   - zenburn low-contrast theme
+;; * Basic and Classical GUI Setting
+;;   - common useless frame elements suppress
+;;   - cursor style & positioning visualization
+;; * Emac Functionality
+;;   - ido/smex/window-switch etc..
+;; * Editting Settings
+;;   - auto-complete/pairing/undo-tree/ace-jump-mode/
+;;     100 column indicator/multiple-cursor
+;; * Programming Settings
+;;   - company mode
+;;   - C/C++ Coding Styles (and very basic settings)
+;;   - Python (very basic settings)
+;; * Extensions
+
+;; --- License:
+;; Copyright (c) 2018-2019 Bingqing Qu
+;; All rights reserved.
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions are
+;; met:
+;;     * Redistributions of source code must retain the above copyright
+;; notice, this list of conditions and the following disclaimer.
+;;     * Redistributions in binary form must reproduce the above copyright
+;; notice, this list of conditions and the following disclaimer in the
+;; documentation and/or other materials provided with the distribution.
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;; A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;; HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;; SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+;; LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+;; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+;; THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+;; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+;; ----------------------------------------------------------------------
+;; Author : Bingqing Qu <sylar.qu@gmail.com>
+;; Version: 0.0
+;; Created: 2017
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Global Operating System Setting
